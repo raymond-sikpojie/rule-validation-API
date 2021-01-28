178855a590e2e2128b63c9738152afc156cc95ee
raymond <raymondomon@yahoo.com>
2021-01-28 04:59:42 +0100

    installed new dependencies

--- server.js
+++ server.js
@@ -1,9 +1,53 @@
 const express = require("express");
 const cors = require("cors");
+const fetch = require("node-fetch")
+// const validator = require("validator")
+const {check, validationResult} = require("express-validator")
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+// Empty array to store data
+let dataArray = [];
+
+// GET route
+app.get("/", (req, res) => {
+
+    res.send({
+        message: "My Rule-Validation API",
+        status: "success",
+        "data":  dataArray[0]
+ 
+        })   
+})
+
+// POST route
+app.post("/validate-rule", [
+    check("name")
+    .notEmpty()
+    .withMessage("Name cannnot be empty")
+    .isLength({min: 5})
+    .withMessage("Username must be at least 5 characters"),
+    check("email")
+    .notEmpty()
+    .withMessage("Email cannot be empty")
+], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        // return res.status(400).send(errors.array().map((item) => {
+        //     return item.msg
+        // }))
+       return res.status(400).send({errors: errors.array()})
+    }
+
+    const {name, github, email, mobile, twitter} = req.body
+    let dataObject = {name, github, email, mobile, twitter}
+    dataArray.push(dataObject)
+    res.send(dataObject)
+})
+
+
 
 // set up server
 const PORT = process.env.PORT || 5000;
