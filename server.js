be84bd15fd615596c5d2eb3e4a8c960f12f559de
raymond <raymondomon@yahoo.com>
2021-01-29 06:45:08 +0100

    added new routes

--- server.js
+++ server.js
@@ -2,50 +2,86 @@ const express = require("express");
 const cors = require("cors");
 const fetch = require("node-fetch")
 // const validator = require("validator")
-const {check, validationResult} = require("express-validator")
+// const {check, validationResult} = require("express-validator")
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-// Empty array to store data
-let dataArray = [];
+const dataArray = []
 
-// GET route
 app.get("/", (req, res) => {
-
-    res.send({
-        message: "My Rule-Validation API",
-        status: "success",
-        "data":  dataArray[0]
- 
-        })   
+    res.send(dataArray[0])
 })
 
-// POST route
-app.post("/validate-rule", [
-    check("name")
-    .notEmpty()
-    .withMessage("Name cannnot be empty")
-    .isLength({min: 5})
-    .withMessage("Username must be at least 5 characters"),
-    check("email")
-    .notEmpty()
-    .withMessage("Email cannot be empty")
-], (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        // return res.status(400).send(errors.array().map((item) => {
-        //     return item.msg
-        // }))
-       return res.status(400).send({errors: errors.array()})
+app.post("/validate-rule", (req, res) => {
+    const {rule, data} = req.body
+
+    if(!rule || !data) {
+        return res.status(400).send("Rule and Data fields are required");
     }
 
-    const {name, github, email, mobile, twitter} = req.body
-    let dataObject = {name, github, email, mobile, twitter}
-    dataArray.push(dataObject)
-    res.send(dataObject)
+    
 })
+
+
+
+
+
+
+
+
+
+
+
+// const express = require("express");
+// const cors = require("cors");
+// const fetch = require("node-fetch")
+// // const validator = require("validator")
+// const {check, validationResult} = require("express-validator")
+
+// const app = express();
+// app.use(express.json());
+// app.use(cors());
+
+// // Empty array to store data
+// let dataArray = [];
+
+// // GET route
+// app.get("/", (req, res) => {
+
+//     res.send({
+//         message: "My Rule-Validation API",
+//         status: "success",
+//         "data":  dataArray[0]
+ 
+//         })   
+// })
+
+// // POST route
+// app.post("/validate-rule", [
+//     check("name")
+//     .notEmpty()
+//     .withMessage("Name cannnot be empty")
+//     .isLength({min: 5})
+//     .withMessage("Username must be at least 5 characters"),
+//     check("email")
+//     .notEmpty()
+//     .withMessage("Email cannot be empty")
+// ], (req, res) => {
+//     const errors = validationResult(req);
+//     if (!errors.isEmpty()) {
+//         // return res.status(400).send(errors.array().map((item) => {
+//         //     return item.msg
+//         // }))
+//        return res.status(400).send({errors: errors.array()})
+//     }
+
+//     const {name, github, email, mobile, twitter} = req.body
+//     let dataObject = {name, github, email, mobile, twitter}
+//     dataArray.push(dataObject)
+//     res.send(dataObject)
+// })
 
 
 
